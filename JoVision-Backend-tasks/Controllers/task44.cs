5e21cfedda75a04f62575909c8284c02fa046181
Abdelrahman <aboud0322@gmail.com>
2024-08-05 22:47:28 +0300

    task44

---
+++ JoVision-Backend-tasks/Controllers/task44.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoVision_Backend_tasks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreeterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = "anonymous")
+        {
+            string greeting = $"hello {name}";
+            return Ok(greeting);
+        }
+    }
+}
