57b7db47b2137c63c387c1d88800a8608cacf403
Abdelrahman <aboud0322@gmail.com>
2024-08-06 19:11:42 +0300

    task45

---
+++ JoVision-Backend-tasks/Controllers/task45_BirthDate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoVision_Backend_tasks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BirthDateController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = "anonymous", [FromQuery] int? years = null, [FromQuery] int? months = null, [FromQuery] int? days = null)
+        {
+            if (years == null || months == null || days == null)
+            {
+                return Ok($"Hello {name}, I can’t calculate your age without knowing your birthdate!");
+            }
+
+            DateTime birthDate = new DateTime(years.Value, months.Value, days.Value);
+            int age = CalculateAge(birthDate);
+
+            return Ok($"Hello {name}, your age is {age}");
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate.Date > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
---
+++ JoVision-Backend-tasks/Controllers/task45_greet.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoVision_Backend_tasks.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GreeterrController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = "anonymous")
+        {
+            string greeting = $"Hello {name}";
+            return Ok(greeting);
+        }
+    }
+}
